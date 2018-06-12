2fd61dcf237c2ce3341c1270845da487ee170190
Hongjun He <anthonyho02015@gmail.com>
2018-06-12 20:02:48 +0000

    starting point

---
+++ portfolio.js
@@ -0,0 +1,44 @@
+/*global $*/
+$( document ).ready(function() {
+    
+
+var aboutMe=$("<div id='about'>");
+var portfolio=$("<div id='about'>");
+var contact=$("<div id='about'>");
+    
+//buttons
+$("#Babout").click(function(){
+    $("#me").toggle();
+});
+
+$("#Bpfo").click(function(){
+    $("#pfo").toggle();
+});
+
+$("#Bcontact").click(function(){
+    $("#contact").toggle();
+});
+    
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    
+    
+});
